6b147a0daaf6599af4c8d06dfe46f021f47e9773
calvinrzachman <calvin.zachman@protonmail.com>
2019-02-08 14:16:02 -0600

    updated App, trying rerun through travis

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
+            <h1>Fibonacci Calculator V2</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
